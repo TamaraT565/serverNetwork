a6de7ca5762e2feb1404300eb5a9f27be68c15c8
Tamara Tatarintseva <tta.nemo@gmail.com>
2023-03-07 22:40:45 +0300

    initial commit

---
+++ src/main/java/org/example/Client.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Client {
+        public static void main(String[] args) {
+            try (Socket socket = new Socket(ServerConfig.Localhost, ServerConfig.Port);
+                 PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
+                 BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+
+                writer.println("It's client");
+                System.out.println(reader.readLine());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
---
+++ src/main/java/org/example/ServerConfig.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class ServerConfig {
+    public static final String Localhost = "127.0.0.1";
+    public static final int Port = 8181;
+}
